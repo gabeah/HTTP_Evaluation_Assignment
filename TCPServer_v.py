d818be04cca4ff217b9540c3a4bff5f4b149e75d
gabeah <94339309+gabeah@users.noreply.github.com>
2024-02-12 13:20:00 -0800

    Initial Upload
    First Upload of code from the assignment

---
+++ TCPServer_v.py
@@ -0,0 +1,23 @@
+from socket import *
+
+# Use this port number
+serverPort = 12000
+# Create a TCP socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Bind our port number to the socket we created
+serverSocket.bind(('', serverPort))
+# Start listening (UDP doesn't do this)
+serverSocket.listen(1)
+
+print('Server is ready to receive')
+while True:
+    # Accept connection (UDP doesn't do this)
+    connectionSocket, addr = serverSocket.accept()
+    # Receive and decode
+    sentence = connectionSocket.recv(1024).decode()
+    # Capitalize as proof of reception
+    capitalizedSentence = sentence.upper()
+    # Send it back
+    connectionSocket.send(capitalizedSentence.encode())
+    # Close the socket
+    connectionSocket.close()
