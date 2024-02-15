3e73e5db65ed71247e7d7c3843b293199df60d30
gabeh <gabeh@penguin>
2024-02-14 19:37:28 -0800

    1c done!

--- UDPClient_v.py
+++ UDPClient_v.py
@@ -2,6 +2,7 @@
 
 # How Python makes it easy to make sockets
 from socket import *
+from socket import timeout
 
 # =+=+=+ Added Imports =+=+=+ #
 import sys
@@ -28,18 +29,23 @@ for i in range(10):
     sentTime = time.time()
     clientSocket.sendto(msg.encode(), (serverName, serverPort))
 
-    clientSocket.settimeout(5) # timeout time of 5 seconds
+    clientSocket.settimeout(1) # timeout time of 5 seconds
 
     try: 
+        RTT = time.time() - sentTime
+        avg_delay.append(RTT)
         modifiedMessage, servAddr = clientSocket.recvfrom(2048)
-        print(modifiedMessage.decode())
-    except socket.timeout:
-        print("Timeout occurred")
+        print(modifiedMessage.decode() + " recieved after " + str(RTT) + " seconds!")
+        successes += 1
+    except TimeoutError:
+        print("Timeout occurred *** !")
 
+avg_rtt = 0.0
 
+for i in avg_delay:
+    avg_rtt += i
 
-
-
+print("The ping succeeded " + str(successes) + " times, with an average RTT of " + str(avg_rtt/successes) + " seconds!")
 
 clientSocket.close()
 
