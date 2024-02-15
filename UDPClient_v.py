7f567e8528ff92a5fa1b78a8be7b53598ca48090
gabeh <gabeh@penguin>
2024-02-14 16:12:44 -0800

    Add 1a,b

--- UDPClient_v.py
+++ UDPClient_v.py
@@ -3,10 +3,13 @@
 # How Python makes it easy to make sockets
 from socket import *
 
+# =+=+=+ Added Imports =+=+=+ #
+import sys
+
 # serverName here works as the IP address
 # serverPort is on what port we will open up our connection
 #serverName = '127.168.0.1' Using localhost for testing on this machine
-serverName = '127.0.0.1'
+serverName = '134.10.133.158'
 serverPort = 12000
 # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
 # This gets Python to create our socket
--- UDPServer_v.py
+++ UDPServer_v.py
@@ -3,9 +3,13 @@
 # How Python makes it easy to make sockets
 from socket import *
 
+# Custom Import
+import sys
+import random
+
 # serverName here works as the IP address
 # serverPort is on what port we will open up our connection
-serverName = '127.0.0.1'
+serverName = '134.10.77.129.'
 serverPort = 12000
 # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
 # This gets Python to create our socket
@@ -15,13 +19,34 @@ serverSocket.bind(('', serverPort))
 # Print message just to check everything happened correctly
 print ("Server is ready to receive")
 # Loop to be continuously listening
+
+''' Old...
 while True:
+
     # We receive, both, message and clientAddress and use 2048 buffer
     message, clientAddress = serverSocket.recvfrom(2048)
     # Modify the message as "proof" we got it at the server, note use of decode()
     modifiedMessage = message.decode().upper()
     # Send the message back, note the use of encode()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+'''
+
+#-+-+- Gabe's Work -+-+-+
+
+droprate = sys.argv[1]
+assert int(droprate) >= 0 and int(droprate) <= 100 
+
+print("Drop Rate is set to " + str(droprate)) + "%")
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+
+    modifiedMessage = message.decode().upper()
+
+    if random.randrange(1, 99, 1) >= droprate:
+        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    else:
+        continue
 
 '''
 part of emily's work
@@ -31,7 +56,7 @@ dropRate = input("Percent of packets to drop (as integer): ") # FIXME: PT2
 
 
 # Loop to be continuously listening
-while True:
+    while True:
     # We receive, both, message and clientAddress and use 2048 buffer
     message, clientAddress = serverSocket.recvfrom(2048)
     # Modify the message as "proof" we got it at the server, note use of decode()
