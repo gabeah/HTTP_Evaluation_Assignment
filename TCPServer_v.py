656491f25cf154cf19bf1a17a5c1db51092c6046
Emily Yu <emilyyu415@Emilys-MacBook-Air-2.local>
2024-02-15 16:53:35 -0800

    emily-work

--- TCPServer_v.py
+++ TCPServer_v.py
@@ -15,32 +15,31 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     # Receive and decode
     sentence = connectionSocket.recv(1024).decode()
-    # Capitalize as proof of reception
-    capitalizedSentence = sentence.upper()
-    # Send it back
-    connectionSocket.send(capitalizedSentence.encode())
-    # Close the socket
-    connectionSocket.close()
-
-'''
-emily's work
-
-while True:
-    # Accept connection (UDP doesn't do this)
-    connectionSocket, addr = serverSocket.accept()
-    # Receive and decode
-    sentence = connectionSocket.recv(1024).decode()
-    req = sentence.split(' ')
+    req = sentence.split()
     
     if req[0].upper() == "GET":
         try:
-            with open(req[1], "r") as req_file:
-                req_file_content = req_file.read()
-                connectionSocket.send(req_file_content.encode()) #FIXME: can i do this...
+            with open("." + req[1], "r", encoding = "UTF-8") as req_file:
+                req_file_content = req_file.read() + "\r\n"
+        
+            # connectionSocket.send(req_file_content.encode()) #FIXME: can i do this...
+                
+            # req
+            connectionSocket.send("HTTP/1.1 200 OK \r\n".encode())
+
+            # header
+            connectionSocket.send("\r\n".encode())
+
+            # content
+            connectionSocket.send(req_file_content.encode())
     
         except FileNotFoundError:
-            error_msg = "404 Not Found"
+            error_msg = "HTTP/1.1 404 Not Found \r\n\r\n"
             connectionSocket.send(error_msg.encode())
+
+    else:
+        error_msg = "HTTP/1.1 400 Bad Request \r\n\r\n"
+        connectionSocket.send(error_msg.encode())
 
 
     '''
@@ -51,4 +50,3 @@ while True:
     # Close the socket
     '''
     connectionSocket.close()
-'''
--- UDPServer_v.py
+++ UDPServer_v.py
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+import random
+
+>>>>>>> 3f1e40c (emily-work)
 # UDP Server
 
 # How Python makes it easy to make sockets
@@ -5,7 +10,10 @@ from socket import *
 
 # serverName here works as the IP address
 # serverPort is on what port we will open up our connection
+<<<<<<< HEAD
 serverName = '127.0.0.1'
+=======
+>>>>>>> 3f1e40c (emily-work)
 serverPort = 12000
 # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
 # This gets Python to create our socket
@@ -14,6 +22,7 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 # Print message just to check everything happened correctly
 print ("Server is ready to receive")
+<<<<<<< HEAD
 # Loop to be continuously listening
 while True:
     # We receive, both, message and clientAddress and use 2048 buffer
@@ -28,6 +37,11 @@ part of emily's work
 
 
 dropRate = input("Percent of packets to drop (as integer): ") # FIXME: PT2
+=======
+
+
+dropRate = int(input("Percent of packets to drop (as integer): ")) # FIXME: PT2
+>>>>>>> 3f1e40c (emily-work)
 
 
 # Loop to be continuously listening
@@ -39,10 +53,17 @@ while True:
 
     #FIXME: PT2
     n = random.randint(1, 100)
+<<<<<<< HEAD
     if (n <= dropRate):
+=======
+    if (n < dropRate):
+>>>>>>> 3f1e40c (emily-work)
         pass
     else:
         # Send the message back, note the use of encode()
         serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+<<<<<<< HEAD
 
 '''
+=======
+>>>>>>> 3f1e40c (emily-work)
